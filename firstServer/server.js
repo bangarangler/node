4346eec4bcd9797da4ec65f83b7866f5c6e30716
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-06 19:30:59 -0400

    pug templating engine example

--- firstServer/server.js
+++ firstServer/server.js
@@ -2,17 +2,20 @@ const path = require("path")
 
 const express = require("express")
 
-const adminRoutes = require('./routes/admin.js')
+const adminData = require('./routes/admin.js')
 const shopRoutes = require('./routes/shop.js')
 
 const bodyParser = require("body-parser")
 
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req,res,next) => {
