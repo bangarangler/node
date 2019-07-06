97b85d69f52bd7ea66870f58fc3a2719e401186b
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-06 16:44:12 -0400

    Express Router... breaking down routes
    if /something/add is going to be used many times ex /something/remove...
      you can break out the /something and place it in the middleware as
      part of the route

--- firstServer/server.js
+++ firstServer/server.js
@@ -1,20 +1,21 @@
 const express = require("express")
 
+const adminRoutes = require('./routes/admin.js')
+const shopRoutes = require('./routes/shop.js')
+
+const bodyParser = require("body-parser")
+
 const app = express()
 
-app.use('/', (req,res,next) => {
-  console.log('This will always run')
-  next()
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req,res,next) => {
+  res.status(404).send("<h1>Page not found</h1>")
 })
 
-app.use('/add-product', (req, res, next) => {
-  console.log(`In another middleware!`)
-  res.send('<h1>The "Add Product" Page</h1>')
-})
 
-app.use('/', (req, res, next) => {
-  console.log(`In another middleware!`)
-  res.send('<h1>hello from express</h1>')
-})
 
 app.listen(3000)
