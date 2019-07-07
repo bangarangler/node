f3a8f56a050aea597d5a3fc8902e352e4e6f6bd8
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-06 21:28:02 -0400

    Using EJS templating engine

--- firstServer/server.js
+++ firstServer/server.js
@@ -2,7 +2,6 @@ const path = require("path")
 
 const express = require("express")
 const bodyParser = require("body-parser")
-const expressHbs = require("express-handlebars")
 
 const adminData = require('./routes/admin.js')
 const shopRoutes = require('./routes/shop.js')
@@ -10,12 +9,7 @@ const shopRoutes = require('./routes/shop.js')
 
 const app = express()
 
-app.engine('hbs', expressHbs({
-  layoutsDir: 'views/layouts/',
-  defaultLayout: 'main-layout',
-  extname: 'hbs',
-}))
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended: false}))
