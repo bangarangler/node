0b1af7ee06c91b4c3d41e2f1b2374aac1096bb37
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-09 10:50:11 -0400

    Able to add products to a cart

--- firstServer/controllers/shop.js
+++ firstServer/controllers/shop.js
@@ -1,61 +1,79 @@
 const Product = require('../models/product.js');
-const Cart = require('../models/cart.js')
+const Cart = require('../models/cart.js');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render("shop/product-list", {
+    res.render('shop/product-list', {
       prods: products,
-      pageTitle: "All Products",
-      path: "/products",
-    })
-  })
+      pageTitle: 'All Products',
+      path: '/products',
+    });
+  });
 };
 
-exports.getProduct = (req,res,next) => {
+exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    res.render('shop/product-detail', { product: product, pageTitle: product.title, path: '/products' })
-  })
-}
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products',
+    });
+  });
+};
 
-exports.getIndex = (req,res,next) => {
+exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render("shop/index", {
+    res.render('shop/index', {
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
+      pageTitle: 'Shop',
+      path: '/',
       hasProducts: products.length > 0,
       activeShop: true,
-      productCSS: true
-    })
-  })
-}
+      productCSS: true,
+    });
+  });
+};
 
-exports.getCart = (req,res,next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  })
-}
+exports.getCart = (req, res, next) => {
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id,
+        );
+        if (cart.products.find(prod => prod.id === product.id)) {
+          cartProducts.push({productData: product, qty: cartProductData.qty});
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
+  });
+};
 
-exports.postCart = (req,res,next) => {
+exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.addProduct(prodId, product.price)
-  })
-  res.redirect('/cart')
-}
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
 
-exports.getOrders = (req,res,next) => {
+exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
-    pageTitle: 'Your Orders'
-  })
-}
+    pageTitle: 'Your Orders',
+  });
+};
 
-exports.getCheckout = (req,res,next) => {
+exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
-    pageTitle: 'Checkout'
-  })
-}
+    pageTitle: 'Checkout',
+  });
+};
