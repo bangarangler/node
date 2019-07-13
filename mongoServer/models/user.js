3d95530732a0db1f53aeee924e99e42ef292939b
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-13 17:08:33 -0400

    manually added user to db for testing

--- mongoServer/models/user.js
+++ mongoServer/models/user.js
@@ -1,22 +1,25 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb');
+const getDb = require('../util/database.js').getDb;
 
-const sequelize = require('../util/database.js')
+const ObjectId = mongodb.ObjectId;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
   }
-})
+
+  save() {
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection('users')
+      .findOne({_id: new ObjectId(userId)});
+  }
+}
 
 module.exports = User;
--- mongoServer/server.js
+++ mongoServer/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error.js');
 const mongoConnect = require('./util/database.js').mongoConnect;
+const User = require('./models/user.js');
 
 const app = express();
 
@@ -18,13 +19,15 @@ const shopRoutes = require('./routes/shop.js');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next) => {
-  //User.findByPk(1).then(user => {
-    //req.user = user;
-    //next()
-  //}).catch(err => console.log(err))
-  next()
-})
+app.use((req, res, next) => {
+  User.findById('5d2a47962074d02a795defbe')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+  next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -33,6 +36,4 @@ app.use(errorController.get404);
 
 mongoConnect(() => {
   app.listen(3000);
-})
-
-
+});
