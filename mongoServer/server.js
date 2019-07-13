4bd0e107062e4daf3d65b8ab4660bd6909ad3b60
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-13 15:45:49 -0400

    Starting to hook covert sequelize mySQL...
    over to Mongodb

--- mongoServer/server.js
+++ mongoServer/server.js
@@ -5,15 +5,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error.js');
-const mongoConnect = require('./util/database.js')
+const mongoConnect = require('./util/database.js').mongoConnect;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-//const adminRoutes = require('./routes/admin.js');
-//const shopRoutes = require('./routes/shop.js');
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,15 +23,15 @@ app.use((req,res,next) => {
     //req.user = user;
     //next()
   //}).catch(err => console.log(err))
+  next()
 })
 
-//app.use('/admin', adminRoutes);
-//app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client) => {
-  console.log(client)
+mongoConnect(() => {
   app.listen(3000);
 })
 
--- mongoServer/util/database.js
+++ mongoServer/util/database.js
@@ -1,17 +1,31 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
 MongoClient.connect(
   `mongodb+srv://${process.env.MONGO_USER}:${
     process.env.MONGO_PW
-  }@clusternodejs-jp-j5zcw.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true }
+  }@clusternodejs-jp-j5zcw.mongodb.net/shop?retryWrites=true&w=majority`, { useNewUrlParser: true }
 )
   .then(client => {
     console.log('Connected!');
-    callback(client);
+    _db = client.db()
+    callback();
   })
-  .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err)
+      throw err;
+    });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No Database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
