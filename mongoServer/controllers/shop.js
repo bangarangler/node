705333f8b60436310ceffe3dde516a425076838a
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-14 12:40:38 -0400

    Orders View back up and working

--- mongoServer/controllers/shop.js
+++ mongoServer/controllers/shop.js
@@ -82,9 +82,9 @@ exports.postOrder = (req, res, next) => {
     .populate('cart.items.productId')
     .execPopulate()
     .then(user => {
-      console.log(user.cart.items)
+      console.log(user.cart.items);
       const products = user.cart.items.map(i => {
-        return {quantity: i.quantity, product: { ...i.productId._doc }};
+        return {quantity: i.quantity, product: {...i.productId._doc}};
       });
       const order = new Order({
         user: {
@@ -96,16 +96,16 @@ exports.postOrder = (req, res, next) => {
       return order.save();
     })
     .then(result => {
-      return req.user.clearCart()
-    }).then(() => {
+      return req.user.clearCart();
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({'user.userId': req.user._id})
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
