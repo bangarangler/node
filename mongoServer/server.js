a8622d3cf77ac8e74771c5af4bf409ad03448925
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-14 09:49:03 -0400

    Prepping to bring in mongoose

--- mongoServer/server.js
+++ mongoServer/server.js
@@ -3,9 +3,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error.js');
-const mongoConnect = require('./util/database.js').mongoConnect;
+//const mongoConnect = require('./util/database.js').mongoConnect;
 const User = require('./models/user.js');
 
 const app = express();
@@ -33,6 +34,15 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PW
+    }@clusternodejs-jp-j5zcw.mongodb.net/shop?retryWrites=true&w=majority`, { useNewUrlParser: true }
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- mongoServer/util/database.js
+++ mongoServer/util/database.js
@@ -1,31 +1,31 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+//const mongodb = require('mongodb');
+//const MongoClient = mongodb.MongoClient;
 
-let _db;
+//let _db;
 
-const mongoConnect = (callback) => {
-MongoClient.connect(
-  `mongodb+srv://${process.env.MONGO_USER}:${
-    process.env.MONGO_PW
-  }@clusternodejs-jp-j5zcw.mongodb.net/shop?retryWrites=true&w=majority`, { useNewUrlParser: true }
-)
-  .then(client => {
-    console.log('Connected!');
-    _db = client.db()
-    callback();
-  })
-    .catch(err => {
-      console.log(err)
-      throw err;
-    });
-}
+//const mongoConnect = (callback) => {
+//MongoClient.connect(
+  //`mongodb+srv://${process.env.MONGO_USER}:${
+    //process.env.MONGO_PW
+  //}@clusternodejs-jp-j5zcw.mongodb.net/shop?retryWrites=true&w=majority`, { useNewUrlParser: true }
+//)
+  //.then(client => {
+    //console.log('Connected!');
+    //_db = client.db()
+    //callback();
+  //})
+    //.catch(err => {
+      //console.log(err)
+      //throw err;
+    //});
+//}
 
-const getDb = () => {
-  if (_db) {
-    return _db;
-  }
-  throw 'No Database found!';
-}
+//const getDb = () => {
+  //if (_db) {
+    //return _db;
+  //}
+  //throw 'No Database found!';
+//}
 
-exports.mongoConnect = mongoConnect;
-exports.getDb = getDb;
+//exports.mongoConnect = mongoConnect;
+//exports.getDb = getDb;
