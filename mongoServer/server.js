bcb370968d73d9765606fb8a8131ec19484e3ce0
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-15 09:14:54 -0400

    Added Login ejs and ...
    slight styling to auth.css
    created new route and a controller for getLogin

--- mongoServer/server.js
+++ mongoServer/server.js
@@ -17,6 +17,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+const authRoutes = require('./routes/auth.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes)
 
 app.use(errorController.get404);
 
