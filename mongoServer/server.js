c593bb778545e059d059d61db47dbcb35a4bc785
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-25 10:28:35 -0400

    Added file add and edit so db gets correct info

--- mongoServer/server.js
+++ mongoServer/server.js
@@ -6,9 +6,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf')
-const flash = require('connect-flash')
-const multer = require('multer')
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const multer = require('multer');
 
 const errorController = require('./controllers/error.js');
 const User = require('./models/user.js');
@@ -20,9 +20,9 @@ const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: 'sessions'
+  collection: 'sessions',
 });
-const csrfProtection = csrf()
+const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -30,8 +30,20 @@ const fileStorage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     cb(null, new Date().toISOString() + '-' + file.originalname);
-  }
+  },
 });
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -41,44 +53,42 @@ const shopRoutes = require('./routes/shop.js');
 const authRoutes = require('./routes/auth.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(multer({ storage: fileStorage }).single('image'))
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
-    store: store
+    store: store,
   }),
 );
 // MUST BE USED AFTER SESSION CREATION
-app.use(csrfProtection)
-app.use(flash())
+app.use(csrfProtection);
+app.use(flash());
 
-app.use(( req,res,next ) => {
+app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken()
-  next()
-})
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
     .then(user => {
       if (!user) {
-        return next()
+        return next();
       }
       req.user = user;
       next();
     })
     .catch(err => {
-      next(new Error(err))
-    })
-})
-
-
+      next(new Error(err));
+    });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -88,14 +98,14 @@ app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
 
-app.use((error, req, res, next)  => {
+app.use((error, req, res, next) => {
   //res.redirect('/500')
   res.status(500).render('500', {
     pageTitle: 'Error!',
     path: '/500',
-    isAuthenticated: req.session.isLoggedIn
-  })
-})
+    isAuthenticated: req.session.isLoggedIn,
+  });
+});
 
 mongoose
   .connect(MONGODB_URI, {useNewUrlParser: true})
