d20577dad07a179f040061a5ae258965eabac85d
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-26 10:33:10 -0400

    Dynamic Pagination in place for shop

--- mongoServer/controllers/shop.js
+++ mongoServer/controllers/shop.js
@@ -1,12 +1,12 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const PDFdocument = require('pdfkit')
+const PDFdocument = require('pdfkit');
 
 const Product = require('../models/product.js');
 const Order = require('../models/order.js');
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -19,9 +19,9 @@ exports.getProducts = (req, res, next) => {
       });
     })
     .catch(err => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -36,29 +36,41 @@ exports.getProduct = (req, res, next) => {
       });
     })
     .catch(err => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page;
+  const page = +req.query.page || 1;
+  let totalItems;
 
   Product.find()
-  .skip((page - 1) * ITEMS_PER_PAGE)
-  .limit(ITEMS_PER_PAGE)
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -75,9 +87,9 @@ exports.getCart = (req, res, next) => {
       });
     })
     .catch(err => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -92,9 +104,9 @@ exports.postCart = (req, res, next) => {
       res.redirect('/cart');
     })
     .catch(err => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -106,9 +118,9 @@ exports.postCartDeleteProduct = (req, res, next) => {
       res.redirect('/cart');
     })
     .catch(err => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -137,9 +149,9 @@ exports.postOrder = (req, res, next) => {
       res.redirect('/orders');
     })
     .catch(err => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -153,54 +165,63 @@ exports.getOrders = (req, res, next) => {
       });
     })
     .catch(err => {
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
-  Order.findById(orderId).then(order => {
-    if (!order) {
-      return next(new Error(`No order found.`))
-    }
-    if (order.user.userId.toString() !== req.user._id.toString()) {
-      return next(new Error('Unauthorized'))
-    }
-    const invoiceName = 'invoice-' + orderId + '.pdf';
-    const invoicePath = path.join(`data`, `invoices`, invoiceName);
+  Order.findById(orderId)
+    .then(order => {
+      if (!order) {
+        return next(new Error(`No order found.`));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error('Unauthorized'));
+      }
+      const invoiceName = 'invoice-' + orderId + '.pdf';
+      const invoicePath = path.join(`data`, `invoices`, invoiceName);
 
-    const pdfDoc = new PDFdocument();
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-    pdfDoc.pipe(fs.createWriteStream(invoicePath));
-    pdfDoc.pipe(res);
+      const pdfDoc = new PDFdocument();
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader(
+        'Content-Disposition',
+        'inline; filename="' + invoiceName + '"',
+      );
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
 
-    pdfDoc.fontSize(26).text('Invoice', {
-      underline: true
-    })
+      pdfDoc.fontSize(26).text('Invoice', {
+        underline: true,
+      });
 
-    pdfDoc.text('--------------------------')
-    let totalPrice = 0;
-    order.products.forEach(prod => {
-      totalPrice += prod.quantity * prod.product.price
-      pdfDoc.fontSize(14).text(`${prod.product.title} - ${prod.quantity} x $${prod.product.price}`)
-    })
-    pdfDoc.text('---')
-    pdfDoc.fontSize(20).text(`Total Price: $${totalPrice}`)
+      pdfDoc.text('--------------------------');
+      let totalPrice = 0;
+      order.products.forEach(prod => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            `${prod.product.title} - ${prod.quantity} x $${prod.product.price}`,
+          );
+      });
+      pdfDoc.text('---');
+      pdfDoc.fontSize(20).text(`Total Price: $${totalPrice}`);
 
-    pdfDoc.end();
+      pdfDoc.end();
 
-    //fs.readFile(invoicePath, (err, data) => {
+      //fs.readFile(invoicePath, (err, data) => {
       //if (err) {
-        //return next(err)
+      //return next(err)
       //}
       //res.setHeader(`Content-Type`, `application/pdf`);
       //res.setHeader("Content-Disposition", 'inline; filename="' + invoiceName + '"')
       //res.send(data);
-    //})
-    //const file = fs.createReadStream(invoicePath);
-    //file.pipe(res);
-  }).catch(err => next(err))
-}
+      //})
+      //const file = fs.createReadStream(invoicePath);
+      //file.pipe(res);
+    })
+    .catch(err => next(err));
+};
