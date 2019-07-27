d129f353f62f5508e7dd899bf377d3cb992f9d95
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-27 15:18:54 -0400

    Added in some dummy data so that I can fetch...
    and see the data on front-end

--- restAPI/controllers/feed.js
+++ restAPI/controllers/feed.js
@@ -1,13 +1,24 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{title: 'First Post', content: 'This is the first post!'}],
+    posts: [
+      {
+        _id: '12',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/deathAndTech.jpg',
+        creator: {
+          name: "Jon",
+        },
+        createdAt: new Date()
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
-  console.log(`title: ${title}, content: ${content}`)
+  console.log(`title: ${title}, content: ${content}`);
   // Create post in db
   res.status(201).json({
     message: 'Post Created Successfully!',
