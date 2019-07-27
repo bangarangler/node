d6b4e9e0d73ee8cbd306e18081bbe7a88e1f0c85
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-27 16:46:55 -0400

    Hooked up URL/ Route for single post...
    found by id

--- restAPI/app.js
+++ restAPI/app.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+
+const path = require('path');
 const express = require('express');
 //const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -10,6 +12,7 @@ const app = express();
 
 //app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 //app.use(cors());
 
 app.use((req, res, next) => {
@@ -23,6 +26,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message })
+})
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@restnode-vrbbu.mongodb.net/messages`, { useNewUrlParser: true }).then(result => {
   app.listen(8080);
--- restAPI/controllers/feed.js
+++ restAPI/controllers/feed.js
@@ -3,31 +3,26 @@ const {validationResult} = require('express-validator');
 const Post = require('../models/post.js');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '12',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/deathAndTech.jpg',
-        creator: {
-          name: 'Jon',
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then(posts => {
+      res
+        .status(200)
+        .json({message: `Fetched posts successfully.`, posts: posts});
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: 'Validation failed, entered data is incorrect.',
-        errors: errors.array(),
-      });
+    const error = new Error(`Validation failed, entered data is incorrect.`);
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
@@ -36,13 +31,38 @@ exports.createPost = (req, res, next) => {
     content: content,
     imageUrl: 'images/deathAndTech.jpg',
     creator: {name: 'Jon'},
-  })
-  post.save().then(result => {
-    res.status(201).json({
-      message: `Post created successfully!`,
-      post: result
+  });
+  post
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: `Post created successfully!`,
+        post: result,
+      });
     })
-  }).catch(err => {
-    console.log(err)
-  })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error(`Could not find post.`);
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({message: `Post fetched.`, post: post});
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
