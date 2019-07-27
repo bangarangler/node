c5491482ea5a7f8ae33daaf0d498afe04511589e
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-27 15:44:48 -0400

    Added some minor validation

--- restAPI/controllers/feed.js
+++ restAPI/controllers/feed.js
@@ -1,3 +1,5 @@
+const {validationResult} = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -7,23 +9,36 @@ exports.getPosts = (req, res, next) => {
         content: 'This is the first post!',
         imageUrl: 'images/deathAndTech.jpg',
         creator: {
-          name: "Jon",
+          name: 'Jon',
         },
-        createdAt: new Date()
+        createdAt: new Date(),
       },
     ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: 'Validation failed, entered data is incorrect.',
+        errors: errors.array(),
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
   //console.log(`title: ${title}, content: ${content}`);
   // Create post in db
   res.status(201).json({
     message: 'Post Created Successfully!',
-    post: {_id: new Date().toISOString(), title: title, content: content, creator: { name: 'Jon' },
-      createdAt: new Date()
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {name: 'Jon'},
+      createdAt: new Date(),
     },
   });
 };
