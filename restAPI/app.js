7d99d1a71f22b4c54c8099a9fbef35b8e8304ed2
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-27 15:07:51 -0400

    Added front-end react code to work with

--- restAPI/app.js
+++ restAPI/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+//const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed.js');
 
@@ -7,6 +8,17 @@ const app = express();
 
 //app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+//app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader(`Access-Control-Allow-Origin`, `*`);
+  res.setHeader(
+    `Access-Control-Allow-Methods`,
+    `OPTIONS, GET, POST, PUT, PATCH, DELETE`,
+  );
+  res.setHeader(`Access-Control-Allow-Headers`, `Content-Type, Authorization`);
+  next();
+});
 
 app.use('/feed', feedRoutes);
 
