d000e6548ac2155adaf066e642ad50ebd93aff91
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-30 10:43:11 -0400

    Added validator package...
    if checks in resolvers for validation starting

--- restAPI/graphql/resolvers.js
+++ restAPI/graphql/resolvers.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs')
+const validator = require('validator')
 
 const User = require('../models/user.js');
 
@@ -6,6 +7,17 @@ module.exports = {
   createUser: async function({ userInput }, req) {
     //const email = args.userInput.email;
     //return User.findOne().then().catch()
+    const errors = []
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: 'Email is invalid.' })
+    }
+    if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, { min: 5 })) {
+      errors.push({ message: 'Password too short!' })
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.')
+      throw error;
+    }
     const existingUser = await User.findOne({ email: userInput.email })
     if (existingUser) {
       const error = new Error('User exists already!')
