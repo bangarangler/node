3aeb7408bee187dc06331c9f57d74a139000ec78
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-30 10:47:43 -0400

    Added custom error fields

--- restAPI/app.js
+++ restAPI/app.js
@@ -56,7 +56,20 @@ app.use((req, res, next) => {
 app.use('/graphql', graphqlhttp({
   schema: graphqlSchema,
   rootValue: graphqlResolver,
-  graphiql: true
+  graphiql: true,
+  formatError(err) {
+    if (!err.originalError) {
+      return err;
+    }
+    const data = err.originalError.data;
+    const message = err.message || 'An error occured.';
+    const code = err.originalError.code || 500;
+    return {
+      message: message,
+      status: code,
+      data: data
+    }
+  }
 }))
 
 //app.use('/feed', feedRoutes);
