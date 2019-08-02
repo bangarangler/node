5303a6324220ed2f669d58e1744886e40267ad04
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-02 15:39:43 -0400

    Working on image upload...
    added a rest endpoint

--- restAPI/app.js
+++ restAPI/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
 //const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -59,6 +60,19 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not Authenticated!')
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: "No file provided!" });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res.status(201).json({ message: 'File stored.', filePath: req.file.path })
+})
+
 app.use(
   '/graphql',
   graphqlhttp({
@@ -108,3 +122,8 @@ mongoose
     //})
   })
   .catch(err => console.log(err));
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
+}
