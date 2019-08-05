ae379419792f74a5a15cabdf36586b01ad72cbdd
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-05 14:58:15 -0400

    manual SSL Encryption with openssl for educational...
    typically hosting provider will handle this for us as browser dosen't
    like our manual set up.

--- mongoServer/server.js
+++ mongoServer/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const fs = require('fs');
+const https = require('https');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -29,6 +30,9 @@ const store = new MongoDBStore({
   collection: 'sessions',
 });
 const csrfProtection = csrf();
+
+const privateKey = fs.readFileSync('server.key');
+const certificate = fs.readFileSync('server.cert');
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -133,7 +137,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI, {useNewUrlParser: true})
   .then(result => {
-    app.listen(process.env.PORT || 3000);
+    https.createServer({ key: privateKey, cert: certificate }, app).listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
