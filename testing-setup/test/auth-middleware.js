46a2da61dc8f39f52430cda1d761c3ecac8e2230
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-06 10:50:15 -0400

    Using mocah and chai for testing
    unit test for auth-middleware

---
+++ testing-setup/test/auth-middleware.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+
+const authMiddleware = require('../middleware/is-auth.js');
+
+// *** UNIT TEST ***
+it('should throw an error if no authorization header is present', function() {
+  const req = {
+    get: function() {
+      return null;
+    }
+  };
+  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated.');
+})
